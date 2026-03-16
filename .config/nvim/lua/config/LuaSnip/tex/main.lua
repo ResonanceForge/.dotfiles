fb58d4401238bc7ffff45020f47609c1bb566602
ResonanceForge <135431383+ResonanceForge@users.noreply.github.com>
2026-03-16 22:12:24 +0800

    remove comment

--- .config/nvim/lua/config/LuaSnip/tex/main.lua
+++ .config/nvim/lua/config/LuaSnip/tex/main.lua
@@ -25,7 +25,6 @@ rec_ls = function()
     return sn(
         nil,
         c(1, {
-            -- Order is important, sn(...) first would cause infinite loop of expansion.
             t(""),
             sn(nil, { t({ "", "\t\\item " }), i(1), d(2, rec_ls, {}) }),
         })
