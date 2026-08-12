541862f9927f8be43ab6a456362217b89e87098b
ResonanceForge <135431383+ResonanceForge@users.noreply.github.com>
2026-08-12 19:58:43 +0800

    Turn off colorizer of neovim

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,7 +1,7 @@
 -- lazy.nvim
 return
 {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = {},
+    -- "catgoose/nvim-colorizer.lua",
+    -- event = "BufReadPre",
+    -- opts = {},
 }
